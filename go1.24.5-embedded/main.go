b36db8d74fcb39558ea585554ee18f81492ff2d8
Michał Derkacz <michal@Lnet.pl>
2025-07-11 13:48:07 +0200

    go1.24.5-embedded

---
+++ go1.24.5-embedded/main.go
@@ -0,0 +1,24 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.24.5-embedded command compiles and runs the go command from Embedded Go
+// 1.24.5
+//
+// To install, run:
+//
+//	$ go install github.com/embeddedgo/dl/go1.24.5-embedded@latest
+//	$ go1.24.5-embedded download
+//
+// Next you can select the newly installed Embedded Go toolchain by setting
+//
+//  GOTOOLCHAIN=go1.24.5-embedded
+//
+// in your environment or use the go1.24.5-embedded command directly.
+package main
+
+import "github.com/embeddedgo/dl/internal/version"
+
+func main() {
+	version.RunTag("go1.24.5-embedded")
+}
